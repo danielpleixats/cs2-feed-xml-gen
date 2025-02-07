f0a03b95ea5d08ba6960e663dfd450121b08febe
danielpleixats <danielplei@gmail.com>
2025-02-07 01:48:51 +0100

    fixes in order to check new entries

--- scripts/update-updates-feed.py
+++ scripts/update-updates-feed.py
@@ -12,40 +12,36 @@ from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
 from datetime import datetime
+import time
 
 base_url = 'https://www.counter-strike.net'
 content_url = base_url + '/news/updates'
 
-# Define a list of languages to fetch and parse
 language_map = {
     'english': ('en', 'en_US'),
     'german': ('de', 'de_DE'),
-    'spanish': ('es', 'es_ES')
+    # 'spanish': ('es', 'es_ES')
 }
 
-# Set up a Chrome WebDriver in headless mode
 try:
     options = Options()
     options.add_argument('--headless')
+    options.add_experimental_option('prefs', {'intl.accept_languages': 'en-US,en;q=0.9'})
+    options.add_argument('--lang=en-US')
     driver = webdriver.Chrome(options=options)
 except:
     sys.exit(f'Failed to initialize the headless web browser.')
 
-# Loop over the languages and generate an RSS feed for each language
 for language_name, (language_code, language_locale) in language_map.items():
-    # Construct the URL for the current language
     url = f'{content_url}?l={language_name}'
 
-    # Launch the browser and navigate to the website to fetch its HTML content
     try:
         driver.get(url)
 
-        # Wait for the contents to appear (thanks Valve for using reactJS)
         element = WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, 'div[class^="blogoverviewpage_SubUpdates"]'))
         )
 
-        # Extract the (hopefully) complete HTML
         html_content = driver.page_source
 
     except TimeoutException:
@@ -53,34 +49,27 @@ for language_name, (language_code, language_locale) in language_map.items():
         sys.exit(f'Unable to find the updates container in the given time frame.')
     except Exception as e:
         driver.quit()
-        sys.exit(f'Failed to extract the HTML data.')
+        sys.exit(f'Failed to extract the HTML data: {e}')
 
-    # Parse the HTML content with BeautifulSoup and extract all relevant information
     soup = BeautifulSoup(html_content, 'html.parser')
-
-    # Find all div containers with class names that contain "updatecapsule_UpdateCapsule"
     capsule_divs = soup.select('div[class*="updatecapsule_UpdateCapsule"]')
 
-    # Create an array of all update items
     updates = []
 
-    # Set locale to parse the date, but dates are currently not localized anyways (Thanks Valve)
-    locale.setlocale(locale.LC_TIME, f'en_US.UTF-8') # Switch to language_locale after it's fixed (if ever)
-    date_format = '%B %d, %Y' # English
- 
-    #locale.setlocale(locale.LC_TIME, 'de_DE') # German
-    #date_format = '%d. %B %Y' # German
+    locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')  # Establecer el locale a inglés
+    date_format = '%B %d, %Y'
 
-    # locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')  # Español de España
-    # date_format = '%d de %B de %Y'
-    
-    # For each update capsule, find all div containers with relevant information
     for capsule in capsule_divs:
         title = capsule.select_one('div[class*="updatecapsule_Title"]').text.strip()
-        date = datetime.strptime(capsule.select_one('div[class*="updatecapsule_Date"]').text.strip(), date_format)
+        date_str = capsule.select_one('div[class*="updatecapsule_Date"]').text.strip() # Obtener el texto de la fecha
+        try:
+          date = datetime.strptime(date_str, date_format)
+        except ValueError as e:
+          print(f"Error al parsear la fecha: {e}. Cadena a parsear: '{date_str}', Formato intentado: '{date_format}'")
+          continue # Saltar al siguiente elemento si la fecha no se puede parsear
+
         desc = capsule.select_one('div[class*="updatecapsule_Desc"]').decode_contents().strip()
 
-        # Remove trailing <br/> tags at the beginning of the update description (Thanks Valve)
         while desc.startswith('<br'):
             index = desc.index('>') + 1
             desc = desc[index:]
@@ -92,23 +81,25 @@ for language_name, (language_code, language_locale) in language_map.items():
             'content': desc
         })
 
-    # Parse an existing RSS feed file and compare the last entry
     github_workspace = os.getenv('GITHUB_WORKSPACE')
-
     if github_workspace:
-        rss_feed_file = os.path.join(os.environ['GITHUB_WORKSPACE'], 'feeds', f'updates-feed-{language_code}.xml')
+        rss_feed_file = os.path.join(github_workspace, 'feeds', f'updates-feed-{language_code}.xml')
     else:
         rss_feed_file = os.path.join(os.pardir, 'feeds', f'updates-feed-{language_code}.xml')
 
-    skip_file = False
+    new_entries = []
 
     if os.path.exists(rss_feed_file):
         current_feed = feedparser.parse(rss_feed_file)
-        if current_feed.entries and updates and current_feed.entries[0].title == updates[0]['title']:
-            skip_file = True
+        existing_guids = {entry.guid for entry in current_feed.entries}
 
-    # Generate the RSS feed with feedgen if the latest entry is different from the current RSS feed
-    if not skip_file:
+        for update in updates:
+            if update['guid'] not in existing_guids:
+                new_entries.append(update)
+    else:
+      new_entries = updates
+
+    if new_entries:
         feed_link = f'https://raw.githubusercontent.com/danielpleixats/cs2-feed-xml-gen/refs/heads/main/feeds/updates-feed-{language_code}.xml'
 
         fg = FeedGenerator()
@@ -117,8 +108,9 @@ for language_name, (language_code, language_locale) in language_map.items():
         fg.link(href=feed_link, rel='self')
         fg.language(language_code)
 
-        # Add the extracted information as entries to the RSS feed
-        for update in reversed(updates):
+        all_updates = new_entries + [update for update in updates if update['guid'] in existing_guids] if os.path.exists(rss_feed_file) else new_entries
+        all_updates.sort(key=lambda x: x['date'], reverse=True)
+        for update in all_updates:
             fe = fg.add_entry()
             fe.source(url)
             fe.guid(update['guid'])
@@ -137,9 +129,12 @@ for language_name, (language_code, language_locale) in language_map.items():
 
         rss_content = fg.rss_str(pretty=True)
 
-        # Create or update XML File
         with open(rss_feed_file, "wb") as f:
             f.write(rss_content)
+
+        print(f"Actualizado el feed {language_code}")
+    else:
+        print(f"No hay nuevas actualizaciones para {language_code}")
 
 driver.quit()
 sys.exit(0)
